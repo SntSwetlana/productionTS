21cdf67cdfac67c3b375d708c6cf60473cef4dd1
SntSwetlana <101958275+SntSwetlana@users.noreply.github.com>
2022-09-29 11:05:03 +0300

    add page loader, add 'not found page'

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import 'app/styles/index.scss';
 import { BrowserRouter } from 'react-router-dom';
-import ThemeProvider, { ThemeProviderProps } from './app/providers/ThemeProvider/ui/ThemeProvider';
+import ThemeProvider from './app/providers/ThemeProvider/ui/ThemeProvider';
 import './shared/config/i18n/i18n';
 import App from './app/App';
+import { ErrorBoundary } from './app/providers/ErrorBoundary';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+// const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
+
     <BrowserRouter>
-        <ThemeProvider>
-            <App />
-        </ThemeProvider>
+        <ErrorBoundary>
+            <ThemeProvider>
+                <App />
+            </ThemeProvider>
+        </ErrorBoundary>
     </BrowserRouter>,
 );
