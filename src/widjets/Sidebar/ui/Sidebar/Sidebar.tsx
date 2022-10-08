348063e5bb38d564106f811ad99ea09a734a1702
SntSwetlana <101958275+SntSwetlana@users.noreply.github.com>
2022-10-08 09:12:26 +0300

    fix toggle tutu

--- src/widjets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widjets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -11,10 +11,11 @@ interface SidebarProps {
 
 export const Sidebar = ({ className }: SidebarProps) => {
     const [collapsed, setCollapsed] = useState(false);
-    const { t } = useTransition(); const
-        onToggle = async () => {
-            setCollapsed((prev) => !prev);
-        };
+    const { t } = useTransition();
+    console.log('tutu');
+    const onToggle = async () => {
+        setCollapsed((prev) => !prev);
+    };
     return (
         <div
             data-testid="sidebar"
