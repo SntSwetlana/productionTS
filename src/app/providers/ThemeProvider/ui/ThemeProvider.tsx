b9fc2054df36767ea65e3c3357cd2251c14721b2
SntSwetlana <101958275+SntSwetlana@users.noreply.github.com>
2022-12-31 00:01:12 +0300

    waiting actions in github

--- src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
+++ src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
@@ -1,14 +1,16 @@
-import React, { FC, useMemo, useState } from 'react';
+import React, { ReactNode, useMemo, useState } from 'react';
 import {
-    LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext, ThemeContextProps,
+    LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext,
 } from '../lib/ThemeContext';
 
-const defaultTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme || Theme.LIGHT;
-export type ThemeProviderProps = {
-    children: React.ReactNode;
+const defaultTheme = (localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme) || Theme.LIGHT;
+interface ThemeProviderProps {
+    initialTheme?: Theme;
+    children: ReactNode;
 }
-const ThemeProvider: React.FC<ThemeProviderProps> = ({ children }) => {
-    const [theme, setTheme] = useState<Theme>(defaultTheme);
+const ThemeProvider = (props: ThemeProviderProps) => {
+    const { initialTheme, children } = props;
+    const [theme, setTheme] = useState<Theme>(initialTheme || defaultTheme);
 
     const defaultProps = useMemo(() => ({
         theme,
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,17 @@
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import ThemeProvider from './app/providers/ThemeProvider/ui/ThemeProvider';
+import { ThemeProvider } from './app/providers/ThemeProvider';
 import App from './app/App';
 import './shared/config/i18n/i18n';
 import { ErrorBoundary } from './app/providers/ErrorBoundary';
 
 const rootElement = document.getElementById('root');
-const root = createRoot(rootElement!);
+if (!rootElement) {
+    throw new Error(
+        'Контейнер root не найден. НЕ удалось вмонтировать реакт приложение',
+    );
+}
+const root = createRoot(rootElement);
 root.render(
     <BrowserRouter>
         <ErrorBoundary>
