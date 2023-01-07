1c85d0b9692ae9e66bc7ba3a8a5cfe400c9cd2fb
SntSwetlana <101958275+SntSwetlana@users.noreply.github.com>
2023-01-07 12:34:24 +0300

    upgrade sidebar add icons and change buttons

--- src/shared/ui/Button/Button.test.tsx
+++ src/shared/ui/Button/Button.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
 import 'app/styles/index.scss';
-import { Button, ThemeButton } from 'shared/ui/Button/Button';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
 
 describe('Button', () => {
     test('test render Button', () => {
@@ -10,7 +10,7 @@ describe('Button', () => {
         screen.debug();
     });
     test('test clear theme', () => {
-        render(<Button theme={ThemeButton.CLEAR}>TEST</Button>);
+        render(<Button theme={ButtonTheme.CLEAR}>TEST</Button>);
         expect(screen.getByText('TEST')).toHaveClass('clear');
         screen.debug();
     });
--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -3,26 +3,46 @@ import React, { ButtonHTMLAttributes, FC } from 'react';
 import '../../../app/styles/index.scss';
 import cls from './Button.module.scss';
 
-export enum ThemeButton{
+export enum ButtonTheme{
     CLEAR = 'clear',
     OUTLINE = 'outline',
+    BACKGROUND = 'background',
+    BACKGROUND_INVERTED = 'backgroundInverted',
 }
+
+export enum ButtonSize{
+    M = 'size_m',
+    L = 'size_l',
+    XL = 'size_xl',
+}
+
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>{
     className?: string;
-    theme?: ThemeButton;
+    theme?: ButtonTheme;
+    square?: boolean;
+    size?: ButtonSize;
 }
 
 export const Button: FC<ButtonProps> = (props) => {
     const {
         className,
         children,
-        theme = ThemeButton.CLEAR,
+        square,
+        size = ButtonSize.M,
+        theme = ButtonTheme.CLEAR,
         ...otherProps
     } = props;
+
+    const mods: Record<string, boolean> = {
+        [cls[theme]]: true,
+        [cls.square]: square,
+        [cls[size]]: true,
+    };
+
     return (
         <button
             type="button"
-            className={classNames(cls.Button, {}, [className, cls[theme]])}
+            className={classNames(cls.Button, mods, [className, cls[theme]])}
             {...otherProps}
         >
             {children}
