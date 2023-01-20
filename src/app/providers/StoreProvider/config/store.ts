07ab2312dba85d9105f9533fbfd7cf4be9218746
SntSwetlana <101958275+SntSwetlana@users.noreply.github.com>
2023-01-20 22:14:48 +0300

    fixed reducers, check ui tests

--- src/app/providers/StoreProvider/config/store.ts
+++ src/app/providers/StoreProvider/config/store.ts
@@ -1,12 +1,16 @@
-import { configureStore, ReducersMapObject } from '@reduxjs/toolkit';
+import { configureStore, DeepPartial, ReducersMapObject } from '@reduxjs/toolkit';
 import { counterReducer } from 'app/entities/Counter';
 import { userReducer } from 'app/entities/User';
 import { loginReducer } from 'features/AuthByUsername/model/slice/loginSlice';
 import { createReducerManager } from './reducerManager';
 import { StateSchema } from './StateSchema';
 
-export function createReduxStore(initialState?: StateSchema) {
+export function createReduxStore(
+    initialState?: StateSchema,
+    asyncReducer?: ReducersMapObject<StateSchema>,
+) {
     const rootReducers: ReducersMapObject<StateSchema> = {
+        ...asyncReducer,
         counter: counterReducer,
         user: userReducer,
     };
