ac0a30f5a82a1a003de70b15781a00c9d4bdbcfb
SntSwetlana <101958275+SntSwetlana@users.noreply.github.com>
2023-01-23 10:21:29 +0300

    add test for login

--- src/widjets/Sidebar/ui/SidebarItem/SidebarItem.tsx
+++ src/widjets/Sidebar/ui/SidebarItem/SidebarItem.tsx
@@ -1,7 +1,5 @@
 import { useTranslation } from 'react-i18next';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
-import { RoutePath } from 'shared/config/routeConfig/routeConfig';
-import MainIcon from 'shared/assets/icons/main-20-20.svg';
 import React, { memo } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { SidebarItemType } from '../../module/items';
