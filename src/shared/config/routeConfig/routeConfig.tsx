7bc8c57a9d7f3ca282506e4a1d46d41bf24ce194
SntSwetlana <101958275+SntSwetlana@users.noreply.github.com>
2023-02-01 00:40:55 +0300

    add article

--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -4,6 +4,8 @@ import { MainPage } from 'pages/MainPage';
 import { AboutPage } from 'pages/AboutPage';
 import { NotFoundPage } from 'pages/NotFoundPage';
 import ProfilePage from 'pages/ProfilePage/ui/ProfilePage';
+import { ArticlePage } from 'pages/ArticlePage';
+import { ArticleDetailsPage } from 'pages/ArticleDetailsPage';
 
 export type AppRoutProps = RouteProps & {
     authOnly?: boolean;
@@ -13,6 +15,8 @@ export enum AppRoutes {
     MAIN = 'main',
     ABOUT = 'about',
     PROFILE = 'profile',
+    ARTICLE = 'article',
+    ARTICLE_DETALES = 'article_details',
     // last
     NOT_FOUND = 'not_found',
 }
@@ -21,6 +25,8 @@ export const RoutePath: Record<AppRoutes, string> = {
     [AppRoutes.MAIN]: '/',
     [AppRoutes.ABOUT]: '/about',
     [AppRoutes.PROFILE]: '/profile',
+    [AppRoutes.ARTICLE]: '/article',
+    [AppRoutes.ARTICLE_DETALES]: '/article/', // + id
     [AppRoutes.NOT_FOUND]: '*',
 };
 
@@ -36,6 +42,16 @@ export const routeConfig: Record<AppRoutes, AppRoutProps> = {
     [AppRoutes.PROFILE]: {
         path: RoutePath.profile,
         element: <ProfilePage />,
+        authOnly: true,
+    },
+    [AppRoutes.ARTICLE]: {
+        path: RoutePath.article,
+        element: <ArticlePage />,
+        authOnly: true,
+    },
+    [AppRoutes.ARTICLE_DETALES]: {
+        path: `${RoutePath.article_details}:id`,
+        element: <ArticleDetailsPage />,
         authOnly: true,
     },
     [AppRoutes.NOT_FOUND]: {
