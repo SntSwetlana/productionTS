8e95928f9bc9c51e06f41a9ab0ad8075af09b1e5
SntSwetlana <101958275+SntSwetlana@users.noreply.github.com>
2023-02-08 07:33:36 +0300

    fix storybook cfg

--- config/storybook/main.js
+++ config/storybook/main.js
@@ -4,9 +4,13 @@ module.exports = {
     ],
     addons: [
         '@storybook/addon-links',
-        '@storybook/addon-essentials',
+        {
+            name: '@storybook/addon-essentials',
+            options: {
+                backgrounds: false,
+            },
+        },
         '@storybook/addon-interactions',
-        ['storybook-addon-react-router-v6'],
     ],
     framework: '@storybook/react',
     core: {
